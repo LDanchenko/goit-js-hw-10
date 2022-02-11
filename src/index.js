33593431192cc314ca9fc02779b096fd06eb98d6
Lubov <2l-u-b-o-v@ukr.net>
2022-02-11 13:41:12 +0200

    added search by input value from backend

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import './css/styles.css';
-import { fetchCountries } from './api/fetchCountries';
+import { fetchCountries } from './js/api/fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
 
-fetchCountries('peru')
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+input.addEventListener(
+  'input',
+  debounce(event => {
+    const inputText = event.target.value.trim();
+    if (inputText) {
+      fetchCountries(inputText)
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }, 300),
+);
--- src/js/api/fetchCountries.js
+++ src/js/api/fetchCountries.js
@@ -4,7 +4,7 @@ export function fetchCountries(name) {
   return fetch(`${BASIC_URL}${name}?fields=name,capital,population,flags,languages`).then(
     response => {
       if (!response.ok) {
-        console.log(response.status);
+        return Promise.reject(response.status);
       }
       return response.json();
     },
